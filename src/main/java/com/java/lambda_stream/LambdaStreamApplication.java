e160214fec85728cdf7b3a84517fc9438b382365
JunalChowdhuryG <chowdhurygomezjunaljohir@gmail.com>
2024-10-21 11:59:13 -0500

    java-lambda-streams

---
+++ src/main/java/com/java/lambda_stream/LambdaStreamApplication.java
@@ -0,0 +1,21 @@
+package com.java.lambda_stream;
+
+import com.java.lambda_stream.principal.Principal;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LambdaStreamApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LambdaStreamApplication.class, args);
+	}
+
+
+	@Override
+	public void run(String... args) throws Exception {
+		Principal principal = new Principal();
+		principal.muestraElMenu();
+	}
+}
